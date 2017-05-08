b62e1e385ea9e172d08bc1a0141910bf37109099
Daniel Feist <daniel.feist@mail.de>
2017-05-08 17:12:05 +0200

    Renamed parallel quicksort

--- quicksort_collection.cpp
+++ quicksort_collection.cpp
@@ -111,7 +111,7 @@ void sequential_quicksort(BiIt first, BiIt last, Pivot_func pivot_func = pivot::
 template<typename BiIt,
          typename Pivot_func = decltype(pivot::random<BiIt>),
          typename Cmp = std::less<>>
-void parallel1_quicksort(BiIt first, BiIt last, int depth = 0, Pivot_func pivot_func = pivot::random, Cmp cmp = Cmp{})
+void naive_parallel_quicksort(BiIt first, BiIt last, int depth = 0, Pivot_func pivot_func = pivot::random, Cmp cmp = Cmp{})
 {
     if (std::distance(first, last) < 2)
         return;
@@ -131,16 +131,16 @@ void parallel1_quicksort(BiIt first, BiIt last, int depth = 0, Pivot_func pivot_
     if (depth < 5)
     {
         std::thread t1([=]() {
-            parallel1_quicksort(first, std::prev(greater_than_pivot), depth+1, pivot_func, cmp);
+            naive_parallel_quicksort(first, std::prev(greater_than_pivot), depth+1, pivot_func, cmp);
         });
 
-        parallel1_quicksort(greater_than_pivot, last, depth+1, pivot_func, cmp);
+        naive_parallel_quicksort(greater_than_pivot, last, depth+1, pivot_func, cmp);
         t1.join();
     }
     else
     {
-        parallel1_quicksort(first, std::prev(greater_than_pivot), depth+1, pivot_func, cmp);
-        parallel1_quicksort(greater_than_pivot, last, depth+1, pivot_func, cmp);
+        naive_parallel_quicksort(first, std::prev(greater_than_pivot), depth+1, pivot_func, cmp);
+        naive_parallel_quicksort(greater_than_pivot, last, depth+1, pivot_func, cmp);
     }
 }
 
@@ -156,7 +156,7 @@ void test_ ## NAME (I first, I last)                                            
 }
 
 TEST_ALGORITHM(sequential)
-TEST_ALGORITHM(parallel1)
+TEST_ALGORITHM(naive_parallel)
 
 int main()
 {
@@ -174,5 +174,5 @@ int main()
     auto inputs = vector<vector<int>> {empty, singleton, doubleton, random, sorted, reversed, almost_sorted, many_unique};
 
     test_sequential(std::begin(inputs), std::end(inputs));
-    test_parallel1(std::begin(inputs), std::end(inputs));
+    test_naive_parallel(std::begin(inputs), std::end(inputs));
 }
